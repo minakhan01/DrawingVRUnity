d033e9be58ffb5a8163b252f89990e69afd6602e
minakhan01 <minakhan01@gmail.com>
2016-06-16 09:46:31 -0400

    getting data from muse bluetooth for drawing

---
+++ Assets/AndroidReceiveMessages.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndroidReceiveMessages : MonoBehaviour {
+
+	private AndroidJavaObject toastExample = null;
+	private AndroidJavaObject activityContext = null;
+
+	// Use this for initialization
+	void Start () {
+		AndroidJNI.AttachCurrentThread();
+		AndroidJNIHelper.debug = true; 
+		if(toastExample == null) {
+			using(AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
+				activityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
+			}
+
+			using(AndroidJavaClass pluginClass = new AndroidJavaClass("com.choosemuse.example.libmuse.MainActivity")) {
+				if(pluginClass != null) {
+//					toastExample = pluginClass.CallStatic<AndroidJavaObject>("instance");
+					toastExample = pluginClass.CallStatic<AndroidJavaObject>("getInstance", activityContext);
+//					activityContext.Call("print", "this");
+				}
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+//		int RValue = 0;
+//				using (AndroidJavaClass javaClass = new AndroidJavaClass("com.choosemuse.example.libmuse.MainActivity")) {
+//			using (AndroidJavaObject activity = javaClass.GetStatic<AndroidJavaObject>("mContext"))
+//			{
+//			Debug.Log("hello");
+//				Debug.Log("rate :"+activity.CallStatic<float>("getRate"));
+//			}
+//		}
+//
+//		//		Debug.Log("rate" + RValue.ToString() );
+	}
+
+	void receiveAlpha(string message) {
+		Debug.Log("message from java: " + message);
+	}
+
+	void receiveEeg(string message) {
+		Debug.Log("message from java: " + message);
+	}
+
+	void receiveAccel(string message) {
+		Debug.Log("message from java: " + message);
+	}
+
+	void receiveStatus(string message) {
+		Debug.Log("message from java: " + message);
+	}
+}
